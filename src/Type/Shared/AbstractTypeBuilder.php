314e10c24166f8db94c4ce2c52d772ea821dd88b
ptlis <ptlis@ptlis.net>
2014-03-16 19:43:51 +0000

    Moved creation of quality factor into factory.

--- src/Type/Shared/AbstractTypeBuilder.php
+++ src/Type/Shared/AbstractTypeBuilder.php
@@ -142,7 +142,7 @@ abstract class AbstractTypeBuilder implements TypeBuilderInterface
     {
         $this->appType = false;
         $this->type = '';
-        $this->qFactor = new QualityFactor(1);
+        $this->qFactor = $this->qFactorFactory->get(0, $this->appType);
     }
 
 
