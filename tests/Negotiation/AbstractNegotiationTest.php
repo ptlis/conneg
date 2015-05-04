a1b820fdb21ac290371d30db9598a76486cd3723
ptlis <ptlis@ptlis.net>
2015-05-04 01:14:28 +0100

    Refactor mime negotiation (best) tests to use data providers

---
+++ tests/Negotiation/AbstractNegotiationTest.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * PHP Version 5.3
+ *
+ * @copyright   (c) 2015 brian ridley
+ * @author      brian ridley <ptlis@ptlis.net>
+ * @license     http://opensource.org/licenses/MIT MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ptlis\ConNeg\Test\Negotiation;
+
+use ptlis\ConNeg\Collection\TypePairCollection;
+use ptlis\ConNeg\Collection\TypePairSort;
+use ptlis\ConNeg\QualityFactor\QualityFactor;
+use ptlis\ConNeg\Type\AbsentType;
+use ptlis\ConNeg\Type\Type;
+use ptlis\ConNeg\Type\WildcardType;
+use ptlis\ConNeg\TypePair\TypePair;
+
+abstract class AbstractNegotiationTest extends \PHPUnit_Framework_TestCase
+{
+    public function mimeProvider()
+    {
+        $sort = new TypePairSort(
+            new TypePair(
+                new AbsentType(new QualityFactor(0)),
+                new AbsentType(new QualityFactor(0))
+            )
+        );
+
+        return array(
+            // There is nothing sensible we can do in this case
+            'user_empty_app_empty' => array(
+                'user' => '',
+                'app' => '',
+                'best' => new TypePair(
+                    new AbsentType(new QualityFactor(0)),
+                    new AbsentType(new QualityFactor(0))
+                ),
+                'all' => new TypePairCollection($sort, array())
+            ),
+
+            // Pair must contain app type with highest quality factor
+            'app_empty' => array(
+                'user' => 'utf-8,iso-8859-5;q=0.75',
+                'app' => '',
+                'best' => new TypePair(
+                    new Type('utf-8', new QualityFactor(1.0)),
+                    new AbsentType(new QualityFactor(0))
+                ),
+                'all' => new TypePairCollection($sort, array())
+            ),
+
+            // Pair must contain user type with highest quality factor
+            'user_empty' => array(
+                'user' => '',
+                'app' => 'iso-8859-1;q=1,utf-8;q=0.5',
+                'best' => new TypePair(
+                    new AbsentType(new QualityFactor(0)),
+                    new Type('iso-8859-1', new QualityFactor(1.0))
+                ),
+                'all' => new TypePairCollection($sort, array())
+            ),
+
+            // When types have matching quality factors the result should be ordered alphabetically - note that this
+            // isn't specification defined, but done to ensure that the sort is stable
+            'user_empty_app_identical_quality' => array(
+                'user' => '',
+                'app' => 'utf-8;q=0.5,iso-8859-1;q=0.5',
+                'best' => new TypePair(
+                    new AbsentType(new QualityFactor(0)),
+                    new Type('iso-8859-1', new QualityFactor(0.5))
+                ),
+                'all' => new TypePairCollection($sort, array())
+            ),
+
+            // Test when we have multiple matching types
+            'multiple_matching_types' => array(
+                'user' => 'windows-1250;q=0.8,utf-8;q=0.3,iso-8859-1;q=0.5',
+                'app' => 'utf-8;q=0.6,iso-8859-5;q=0.9,iso-8859-1;q=0.3',
+                'best' => new TypePair(
+                    new Type('utf-8', new QualityFactor(0.3)),
+                    new Type('utf-8', new QualityFactor(0.6))
+                ),
+                'all' => new TypePairCollection($sort, array())
+            ),
+
+            // Test wildcards - the wildcard match has higher quality factor product
+            'test_wildcard_qualities' => array(
+                'user' => 'iso-8859-5;q=0.3,utf-8;q=0.9,*;q=0.5',
+                'app' => 'iso-8859-5,windows-1250',
+                'best' => new TypePair(
+                    new WildcardType(new QualityFactor(0.5)),
+                    new Type('windows-1250', new QualityFactor(1))
+                ),
+                'all' => new TypePairCollection($sort, array())
+            )
+        );
+    }
+}
--- tests/Negotiation/CharsetBestTest.php
+++ tests/Negotiation/CharsetBestTest.php
@@ -1,8 +1,6 @@
 <?php
 
 /**
- * Test to verify the use of the Negotiate::charsetBest method.
- *
  * PHP Version 5.3
  *
  * @copyright   (c) 2006-2015 brian ridley
@@ -15,129 +13,25 @@
 
 namespace ptlis\ConNeg\Test\Negotiation;
 
+use ptlis\ConNeg\Collection\CollectionInterface;
 use ptlis\ConNeg\Negotiation;
-use ptlis\ConNeg\QualityFactor\QualityFactor;
-use ptlis\ConNeg\Type\AbsentType;
-use ptlis\ConNeg\Type\Type;
-use ptlis\ConNeg\TypePair\TypePair;
+use ptlis\ConNeg\Type\TypeInterface;
 
-class CharsetBestTest extends \PHPUnit_Framework_TestCase
+class CharsetBestTest extends AbstractNegotiationTest
 {
-    public function testUserAndAppEmpty()
+    /**
+     * @dataProvider mimeProvider
+     *
+     * @param string $userField
+     * @param string $appField
+     * @param TypeInterface $best
+     * @param CollectionInterface $all
+     */
+    public function testBest($userField, $appField, TypeInterface $best, CollectionInterface $all)
     {
-        $httpField  = '';
-        $appPrefs   = '';
-
-        $expectPair = new TypePair(
-            new AbsentType(new QualityFactor(0)),
-            new AbsentType(new QualityFactor(0))
-        );
-
         $negotiate = new Negotiation();
-        $resultType = $negotiate->charsetBest($httpField, $appPrefs);
+        $resultType = $negotiate->charsetBest($userField, $appField);
 
-        $this->assertEquals($expectPair, $resultType);
-    }
-
-
-    public function testAppEmpty()
-    {
-        $httpField  = 'utf-8,iso-8859-5;q=0.75';
-        $appPrefs   = '';
-
-        $expectPair = new TypePair(
-            new Type('utf-8', new QualityFactor(1)),
-            new AbsentType(new QualityFactor(0))
-        );
-
-        $negotiate = new Negotiation();
-        $resultType = $negotiate->charsetBest($httpField, $appPrefs);
-
-        $this->assertEquals($expectPair, $resultType);
-    }
-
-
-    public function testUserEmpty()
-    {
-        $httpField  = '';
-        $appPrefs   = 'iso-8859-1;q=1,utf-8;q=0.5';
-
-        $expectPair = new TypePair(
-            new AbsentType(new QualityFactor(0)),
-            new Type('iso-8859-1', new QualityFactor(1))
-        );
-
-        $negotiate = new Negotiation();
-        $resultType = $negotiate->charsetBest($httpField, $appPrefs);
-
-        $this->assertEquals($expectPair, $resultType);
-    }
-
-
-    public function testUserEmptyAppSameQuality()
-    {
-        $httpField  = '';
-        $appPrefs   = 'utf-8;q=0.5,iso-8859-1;q=0.5';
-
-        $expectPair = new TypePair(
-            new AbsentType(new QualityFactor(0)),
-            new Type('iso-8859-1', new QualityFactor(0.5))
-        );
-
-        $negotiate = new Negotiation();
-        $resultType = $negotiate->charsetBest($httpField, $appPrefs);
-
-        $this->assertEquals($expectPair, $resultType);
-    }
-
-
-    public function testUserAppTypesIdentical()
-    {
-        $httpField  = 'utf-8;q=0.6,iso-8859-5;q=0.9';
-        $appPrefs   = 'iso-8859-5;q=0.9,utf-8;q=0.6';
-
-        $expectPair = new TypePair(
-            new Type('iso-8859-5', new QualityFactor(0.9)),
-            new Type('iso-8859-5', new QualityFactor(0.9))
-        );
-
-        $negotiate = new Negotiation();
-        $resultType = $negotiate->charsetBest($httpField, $appPrefs);
-
-        $this->assertEquals($expectPair, $resultType);
-    }
-
-
-    public function testUserAppQualityFactorIntersection()
-    {
-        $httpField  = 'utf-8;q=0.6,iso-8859-5;q=0.9,iso-8859-1;q=0.3';
-        $appPrefs   = 'windows-1250;q=0.8,utf-8;q=0.3,iso-8859-1;q=0.5';
-
-        $expectPair = new TypePair(
-            new Type('utf-8', new QualityFactor(0.6)),
-            new Type('utf-8', new QualityFactor(0.3))
-        );
-
-        $negotiate = new Negotiation();
-        $resultType = $negotiate->charsetBest($httpField, $appPrefs);
-
-        $this->assertEquals($expectPair, $resultType);
-    }
-
-
-    public function testWildcard()
-    {
-        $httpField  = 'iso-8859-5;q=0.8,utf-8;q=0.9,*;q=0.5';
-        $appPrefs   = 'iso-8859-5,utf-8;q=0.7,windows-1250;q=0.3';
-
-        $expectPair = new TypePair(
-            new Type('iso-8859-5', new QualityFactor(0.8)),
-            new Type('iso-8859-5', new QualityFactor(1))
-        );
-
-        $negotiate = new Negotiation();
-        $resultType = $negotiate->charsetBest($httpField, $appPrefs);
-
-        $this->assertEquals($expectPair, $resultType);
+        $this->assertEquals($best, $resultType);
     }
 }
