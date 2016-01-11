04afb568f368ecdd81c13277006c4be041ccb58b
brian ridley <ptlis@ptlis.net>
2016-01-11 21:31:04 +0000

    Update copyright comments for the final time

--- src/Negotiator/Negotiator.php
+++ src/Negotiator/Negotiator.php
@@ -1,14 +1,9 @@
 <?php
 
 /**
- * PHP Version 5.3
- *
- * @copyright   (c) 2006-2015 brian ridley
+ * @copyright   (c) 2006-present brian ridley
  * @author      brian ridley <ptlis@ptlis.net>
  * @license     http://opensource.org/licenses/MIT MIT
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
  */
 
 namespace ptlis\ConNeg\Negotiator;
